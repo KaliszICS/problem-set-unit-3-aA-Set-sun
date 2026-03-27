ee2d8f1f810e6ce8d8bdf17efa41deba52a07b70
aA-Set-sun <343303426@tdsb.ca>
2026-03-27 14:07:18 -0400

    Update ProblemSet.java

--- src/main/java/ProblemSet.java
+++ src/main/java/ProblemSet.java
@@ -1,7 +1,26 @@
+/* 
+Title: Problem Set Unit 3
+Name: Angie
+Date created: March 27, 2026
+Date updated: March 27, 2026
+
+Just lost all my progress :(
+*/
+
+import java.util.Scanner; 
+
 public class ProblemSet {
 
 	public static void main(String args[]) {
 
+		Scanner input = new Scanner(System.in);
+		String email = input.nextLine(); 
+		
+	}
+
+	public static String emailValidation (String email) {
+		
+	
 	}
 
 }
