21c9aa91583d19fe76de323de9eddd984ed29236
aA-Set-sun <343303426@tdsb.ca>
2026-04-02 18:44:11 +0000

    um what now

--- src/main/java/ProblemSet.java
+++ src/main/java/ProblemSet.java
@@ -12,26 +12,31 @@ import java.util.Scanner;
 public class ProblemSet {
 
 	public static void main(String args[]) {
-
+	
 		Scanner input = new Scanner(System.in);
 		
 		System.out.print("Input an email: ");
 		String email = input.nextLine(); 
-		email = email.trim();
+		email = email.trim();	
+		
+		System.out.println(emailValidation(email));
+
+	}
+
+	public static String emailValidation (String email) {
 
 		String local;
 		String domain;
-		String subDomain;
 		String domainExtension;
 
 		if (email.length() != 0) {
+			if ( email.contains("@") && email.matches("@")) { //contains @ check
 
-			local = email.substring(0, email.indexOf("@"));
+				local = email.substring(0, email.indexOf("@"));
 
-			domain = email.substring(email.indexOf("@") + 1, email.length());
-			domainExtension = domain.substring((domain.lastIndexOf(".") + 1), domain.length());
+				domain = email.substring(email.indexOf("@") + 1, email.length());
+				domainExtension = domain.substring((domain.lastIndexOf(".") + 1), domain.length());
 
-			if (email.contains("@")) { //contains @ check
 				if (!(email.startsWith(".") || email.endsWith(".") || email.startsWith("+") || email.endsWith("+") || email.startsWith("_") || email.endsWith("_"))) { //starts + ends with dot check (+ THE OTHER SYMBOLS)
 					if (!(email.contains(" "))) { //space check
 						if (local.length() >= 1 && local.length() <= 64) { //length check
@@ -39,41 +44,28 @@ public class ProblemSet {
 								if (domainExtension.length() >= 2 && domainExtension.length() <= 6) { //domain extension length + checks for subdomains
 									if (local.contains(".")) { //exception C
 										local = local.replace(".", ""); //removed dots in local
-										System.out.println("Valid (Gmail normalized)");
+										return "Valid (Gmail normalized)";
 									}
-									else {
-										System.out.println("Valid");
-									}
+									return "Valid";
 								}
-								else {
-									System.out.println("Invalid: Invalid domain extension length");
-								}
+								return "Invalid: Invalid domain extension length";
 							}
-							else {
-								System.out.println("Invalid: Domain contains no dot");
-							}
+							return "Invalid: Domain contains no dot";
+						
 						}
-						else {
-						System.out.println("Invalid: Invalid local length");
-						}
-					}
-					else {
-						System.out.println("Invalid: Contains spaces");
-					}
+						return "Invalid: Invalid local length";
+						
+					}					
+					return "Invalid: Contains spaces";
+					
 				}
-				else {
-					System.out.println("Invalid: Starts or ends with dot, plus, or underscore");
-				}
-
-			}
-			else {
-				System.out.println("Invalid: Missing @");
-			}
+				return "Invalid: Starts or ends with dot, plus, or underscore";
 	
+			}
+			return "Invalid: Missing @ or too many @s";
+		
 		}
-		else {
-			System.out.println("Invalid: Nothing was entered");
-		}
+		return "Invalid: Nothing was entered";
 
 }
 }
