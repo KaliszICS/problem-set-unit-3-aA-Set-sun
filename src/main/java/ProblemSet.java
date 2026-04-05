bc2fabb0c16d84a60cd9aa8e6aeeba7205d56260
aA-Set-sun <343303426@tdsb.ca>
2026-04-05 18:17:52 +0000

    i think we did something

--- src/main/java/ProblemSet.java
+++ src/main/java/ProblemSet.java
@@ -30,49 +30,83 @@ public class ProblemSet {
 
 	public static String emailValidation (String email) {
 
-		/* String local;
+		String local;
 		String domain;
 		String domainExtension;
 
 		if (email.length() != 0) {
-			if ( email.contains("@") && email.matches("@")) { //contains @ check
+			if (email.matches("@")) { //contains @ check
 
 				local = email.substring(0, email.indexOf("@"));
-
 				domain = email.substring(email.indexOf("@") + 1, email.length());
 				domainExtension = domain.substring((domain.lastIndexOf(".") + 1), domain.length());
 
-				*/
-
-				if (!(email.startsWith(".") || email.endsWith(".") || email.startsWith("+") || email.endsWith("+") || email.startsWith("_") || email.endsWith("_"))) { //starts + ends with dot check (+ THE OTHER SYMBOLS)
-					if (!(email.contains(" "))) { //space check
-						if (local.length() >= 1 && local.length() <= 64) { //length check
-							if (domain.contains(".")) { //domain contains at least ONE (1) dot
-								if (domainExtension.length() >= 2 && domainExtension.length() <= 6) { //domain extension length + checks for subdomains
-									if (local.contains(".")) { //exception C
-										local = local.replace(".", ""); //removed dots in local
-										return "Valid (Gmail normalized)";
-									}
-									return "Valid";
-								}
-								return "Invalid: Invalid domain extension length";
-							}
-							return "Invalid: Domain contains no dot";
-						
-						}
-						return "Invalid: Invalid local length";
-						
-					}					
-					return "Invalid: Contains spaces";
 					
-				}
-				return "Invalid: Starts or ends with dot, plus, or underscore";
-	/*
 			}
 			return "Invalid: Missing @ or too many @s";
 		
 		}
 		return "Invalid: Nothing was entered";
-*/
-}
-}
+
+
+				
+	}
+
+	public static String emailValidation2 (String email, String local, String domain, String domainExtension) {
+	
+		if (email.startsWith(".") || email.endsWith(".") || email.startsWith("+") || email.endsWith("+") || email.startsWith("_") || email.endsWith("_")) { //starts + ends with dot check (+ THE OTHER SYMBOLS)
+			return "Invalid: Starts or ends with dot, plus, or underscore";
+		}
+
+		if (email.contains(" ")) { //space check
+			return "Invalid: contains spaces";
+		}
+
+		if (!((local.length() >= 1) && local.length() <= 64)) { //length check
+			return "Invalid: Invalid local length";
+		}
+
+		if (!(domain.contains("."))) { //contains at least ONE (1) dot
+			return "Invalid: Domain contains no dot";
+		}
+
+		if (!(domainExtension.length() >= 2 && domainExtension.length() <= 6)) { //domain extension length
+			return "Invalid: Invalid domain extension length";
+		} 
+
+		if (local.contains(".")) {
+			local = local.replace(".", ""); //removed dots in local
+			return "Valid (Gmail normalized)";
+		}
+		else {
+			return "Valid";
+		}
+
+	}
+	}
+	//if (!(email.startsWith(".") || email.endsWith(".") || email.startsWith("+") || email.endsWith("+") || email.startsWith("_") || email.endsWith("_"))) { //starts + ends with dot check (+ THE OTHER SYMBOLS)
+// 					if (!(email.contains(" "))) { //space check
+// 						if (local.length() >= 1 && local.length() <= 64) { //length check
+// 							if (domain.contains(".")) { //domain contains at least ONE (1) dot
+// 								if (domainExtension.length() >= 2 && domainExtension.length() <= 6) { //domain extension length + checks for subdomains
+// 									if (local.contains(".")) { //exception C
+// 										local = local.replace(".", ""); //removed dots in local
+// 										return "Valid (Gmail normalized)";
+// 									}
+// 									return "Valid";
+// 								}
+// 								return "Invalid: Invalid domain extension length";
+// 							}
+// 							return "Invalid: Domain contains no dot";
+						
+// 						}
+// 						return "Invalid: Invalid local length";
+						
+// 					}					
+// 					return "Invalid: Contains spaces";
+					
+// 				}
+// 				return "Invalid: Starts or ends with dot, plus, or underscore";
+
+// }
+	
